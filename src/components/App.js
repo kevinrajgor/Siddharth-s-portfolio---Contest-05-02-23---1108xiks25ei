73c84e88dbc9c6eba30212272f1b37c052c731eb
Kevin Rajgor <kevinrajg95@gmail.com>
2023-02-05 16:22:55 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,35 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
 // import { BrowserRouter as Router, Route,Link,Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect, NavLink, Link } from 'react-router-dom';
-import Info from './Info';
-import Blog from './Blog';
-import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  NavLink,
+  Link,
+} from "react-router-dom";
+import Info from "./Info";
+import Blog from "./Blog";
+import Home from "./Home";
 const App = () => {
-
   return (
     <div id="main">
-
-
       <Router>
         <Switch>
           <Route path="/" exact>
             <Home />
           </Route>
-          <route path='/blog' exact>
+          <Route path="/blog">
             <Blog />
-          </route>
-          <route path='/info' exact>
+          </Route>
+          <Route path="/Info">
             <Info />
-          </route>
+          </Route>
         </Switch>
       </Router>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 const Home = () => {
-
-    return (
-        <div id='home-page'>
-
-            <h1>Home page</h1>
-            <h2>Hi, my name is Siddharth </h2>
-            <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
-
-        </div>
-    )
-}
-export default Home
+  return (
+    <div id="home-page">
+      <h1>Home page</h1>
+      <h2>Hi, my name is Siddharth </h2>
+      <h2>
+        and I work at <a href="https://www.newtonschool.co/">Newton School</a>
+      </h2>
+      <button id="home-to-blog">
+        {" "}
+        <Link to="/blog"> Go to Blog</Link>
+      </button>
+      <button id="home-to-info">
+        {" "}
+        <Link to="/Info">Go to Info</Link>
+      </button>
+    </div>
+  );
+};
+export default Home;
